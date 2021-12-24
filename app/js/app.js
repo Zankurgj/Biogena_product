ee3132660786cf931e6246f4795e75aa95c20183
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2021-12-24 13:59:17 +0300

    remove

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-  spoilerBtnListener();
+  $(".js--popular-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    dots: false,
+    touchThreshold: 20,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+        },
+      },
+    ],
+  });
+  $(".js--btn-like").on("click", function (evt) {
+    $(evt.currentTarget).toggleClass("active");
+  });
+  scrollTopBtnInit();
 });
 
-function spoilerBtnListener() {
-  $(".js--spoiler-btn").on("click", spoilerHandler);
-}
-
-function spoilerHandler(evt) {
-  $(evt.currentTarget).toggleClass("opened");
-  $(evt.currentTarget).siblings(".js--spoiler-content").slideToggle(200);
-}
+const scrollTopBtnInit = () => {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".js--btn-top").addClass("show");
+    } else {
+      $(".js--btn-top").removeClass("show");
+    }
+  });
+  $(".js--btn-top").click(() => {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+};
